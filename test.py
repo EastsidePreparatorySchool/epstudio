acc8b1c6ffbdb73c5d13b18ffda887df976f15f4
shru-shrux <shruti.kancherla@gmail.com>
2024-09-24 16:45:45 -0700

    updated test

--- test.py
+++ test.py
@@ -1,7 +1,12 @@
 from flask import Flask
 
-app = Flask(__name__)
+test = Flask(__name__) # needs to match file name
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@test.route('/')
+def hello():
+    return '<h1>Hello, World!</h1>'
+
+# put these lines in the terminal then click on link to open webpage
+# set FLASK_APP=test
+# set FLASK_ENV=development
+# flask run
